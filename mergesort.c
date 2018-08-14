50271239b1f996d6aa20989379cf243da7253ae4
nataliabarreto <nataliaabarretoo@hotmail.com>
2018-08-14 14:39:51 -0300

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,71 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+import time
 
-void mergesort(int *v, int n);
-void sort(int *v, int *c, int i, int f);
-void merge(int *v, int *c, int i, int m, int f);
+def merge(list1):
+    if len(list1)>1:
+        mid = len(list1)//2
+        lefthalf = list1[:mid]
+        righthalf = list1[mid:]
 
-int main (void) {
-  int i;
-  int v[8] = { -1, 7, -3, 11, 4, -2, 4, 8 };
+        merge(lefthalf)
+        merge(righthalf)
 
-  mergesort(v, 8);
+        i=0
+        j=0
+        k=0
 
-  for (i = 0; i < 8; i++) printf("%d ", v[i]);
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                list1[k]=lefthalf[i]
+                i=i+1
+            else:
+                list1[k]=righthalf[j]
+                j=j+1
+            k=k+1
 
-  putchar('\n');
+        while i < len(lefthalf):
+            list1[k]=lefthalf[i]
+            i=i+1
+            k=k+1
 
-  return 0;
-}
+        while j < len(righthalf):
+            list1[k]=righthalf[j]
+            j=j+1
+            k=k+1
 
-/*
-  Dado um vetor de inteiros v e um inteiro n >= 0, ordena o vetor v[0..n-1] em ordem crescente.
-*/
-void mergesort(int *v, int n) {
-  int *c = malloc(sizeof(int) * n);
-  sort(v, c, 0, n - 1);
-  free(c);
-}
+f = open('num.1000.1.in', 'r')
+vetor = f.readlines()
+antes = time.time()
+merge (vetor)
+depois = time.time()
+total = (depois - antes) *1000
+            
+print(vetor)
 
-/*
-  Dado um vetor de inteiros v e dois inteiros i e f, ordena o vetor v[i..f] em ordem crescente.
-  O vetor c é utilizado internamente durante a ordenação.
-*/
-void sort(int *v, int *c, int i, int f) {
-  if (i >= f) return;
-
-  int m = (i + f) / 2;
-
-  sort(v, c, i, m);
-  sort(v, c, m + 1, f);
-
-  /* Se v[m] <= v[m + 1], então v[i..f] já está ordenado. */
-  if (v[m] <= v[m + 1]) return;
-
-  merge(v, c, i, m, f);
-}
-
-
-/*
-  Dado um vetor v e três inteiros i, m e f, sendo v[i..m] e v[m+1..f] vetores ordenados,
-  coloca os elementos destes vetores, em ordem crescente, no vetor em v[i..f].
-*/
-void merge(int *v, int *c, int i, int m, int f) {
-  int z,
-      iv = i, ic = m + 1;
-
-  for (z = i; z <= f; z++) c[z] = v[z];
-
-  z = i;
-
-  while (iv <= m && ic <= f) {
-    /* Invariante: v[i..z] possui os valores de v[iv..m] e v[ic..f] em ordem crescente. */
-
-    if (c[iv] < c[ic]) v[z++] = c[iv++];
-    else /* if (c[iv] > c[ic]) */ v[z++] = c[ic++];
-  }
-
-  while (iv <= m) v[z++] = c[iv++];
-
-  while (ic <= f) v[z++] = c[ic++];
-}
+print("O tempo total foi: %0.2f ms" % total) 
